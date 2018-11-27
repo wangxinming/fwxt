1368c2e7bb4a9193e31332b3825d1ee8ff4f2598
wangxinming <wangxm@fosun.com>
2018-11-27 10:34:54 +0800

    测试数据清理

---
+++ src/main/resources/sql/clean.sql
@@ -0,0 +1,26 @@
+use oa;
+delete  from ACT_GE_BYTEARRAY;
+
+delete  from ACT_HI_ACTINST;
+delete  from ACT_HI_ATTACHMENT;
+delete  from ACT_HI_COMMENT;
+delete  from ACT_HI_DETAIL;
+delete  from ACT_HI_IDENTITYLINK;
+delete  from ACT_HI_PROCINST;
+delete  from ACT_HI_TASKINST;
+delete  from ACT_HI_VARINST;
+
+delete  from ACT_RU_EVENT_SUBSCR;
+delete  from ACT_RU_EXECUTION;
+delete  from ACT_RU_IDENTITYLINK;
+delete  from ACT_RU_JOB;
+delete  from ACT_RU_TASK;
+delete  from ACT_RU_VARIABLE;
+
+
+delete  from OA_ATTACHMENT;
+delete  from OA_AUDIT;
+delete  from OA_CONTRACT_CIRCULATION;
+delete  from OA_NOTIFY;
+
+
